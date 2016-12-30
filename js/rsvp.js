e094183de5963d0124968c0df539f753167caa39
Chad Knight <chadrknight@gmail.com>
2016-12-30 14:37:10 -0600

    revamped + rsvp

---
+++ js/rsvp.js
@@ -0,0 +1,40 @@
+  // jQuery snippet for changing HTML form into JSON
+  (function ($) {
+    $.fn.serializeFormJSON = function () {
+      var o = {};
+      var a = this.serializeArray();
+      $.each(a, function () {
+        if (o[this.name]) {
+          if (!o[this.name].push) { o[this.name] = [o[this.name]]; }
+          o[this.name].push(this.value || '');
+        } else { o[this.name] = this.value || ''; }
+      });
+      return o;
+    };
+  })(jQuery);
+
+  $('#rsvp-form').submit(function(e) {
+    // prevent default submiting form
+    e.preventDefault();
+
+    // serialize data to JSON
+    var data = $('#rsvp-form').serializeFormJSON();
+
+    $.ajax({
+      url: 'https://sheetsu.com/apis/v1.0/705b4743378b',
+      data: data,
+      dataType: 'json',
+      type: 'POST',
+
+      success: function(data) {
+        window.location.href = 'thanks';
+      },
+
+      // handling error response
+      error: function(data) {
+        console.log(data);
+      }
+    });
+
+    return false;
+  });
